fba8cc0d1571e8679fdda0924b069f1dbd397cdf
Rico Rojas <djizco@yahoo.com>
2016-11-22 20:09:44 -0600

    Update navbar styles

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router';
 
 export default function Navigation() {
   return (
-    <div className="main-header nav">
+    <nav id="header" className="nav">
       <div className="nav-left">
         <Link to="/" activeStyle={{ textDecoration: 'none', color: '#DDD' }} className="nav-item is-brand">
-          <h2 className="margin-sm">App</h2>
+          <h3 className="title is-3">Electron</h3>
         </Link>
       </div>
-    </div>
+    </nav>
   );
 }
