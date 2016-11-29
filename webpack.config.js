9ffbc3be7abd44dcbcdffaf9392c823413bb7b6c
Rico Rojas <djizco@yahoo.com>
2016-11-29 00:44:52 -0600

    Add trailing commas

--- webpack.config.js
+++ webpack.config.js
@@ -12,50 +12,50 @@ const ExtractTextPluginConfig = new ExtractTextPlugin('bundle.css');
 
 module.exports = {
   entry: [
-    './app/index.js'
+    './app/index.js',
   ],
   output: {
+    filename: 'bundle.js',
     path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', 'css')
+        loader: ExtractTextPlugin.extract('style', 'css'),
       },
       {
         test: /\.scss$/i,
-        loader: ExtractTextPlugin.extract(['css', 'sass'])
+        loader: ExtractTextPlugin.extract(['css', 'sass']),
       },
       {
         test: /\.less$/i,
-        loader: ExtractTextPlugin.extract(['css', 'less'])
+        loader: ExtractTextPlugin.extract(['css', 'less']),
       },
       {
         test: /\.(jpe?g|png|gif|svg|ico)$/i,
         loaders: [
           'file?name=[name].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
         ]
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
-      }
-    ]
+        loader: 'file-loader',
+      },
+    ],
   },
   plugins: [
     HtmlWebpackPluginConfig,
     ExtractTextPluginConfig,
-  ]
+  ],
 };
